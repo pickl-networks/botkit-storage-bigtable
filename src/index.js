85057725aa6307f48ced56cc41d3a154ceba1271
Simon Morley <simon@polkaspots.com>
2017-08-26 18:52:35 +0100

    comments

--- src/index.js
+++ src/index.js
@@ -1,47 +1,60 @@
-var bigtable = require('@google-cloud/bigtable')({
-  projectId: process.env.PROJECT_ID || 'emulator-project-id',
-});
-
 var Q = require('q');
+
+// var instance, table; //= bigtable.instance('instance-1');
+// var table = instance.table('PCKLISS');
 
 // var gcloud = require('google-cloud');
 
-/**
- * The Botkit google cloud bigtable driver
- *
- * @param {Object} config This must contain a `projectId` property
- * @returns {{teams: {get, save, all}, channels: {get, save, all}, users: {get, save, all}}}
- */
+  /**
+   * The Botkit google cloud bigtable driver
+   *
+   * @param {Object} config This must contain a `projectId` property
+   * @returns {{teams: {get, save, all}, channels: {get, save, all}, users: {get, save, all}}}
+   */
+
+var bigtable;
+
 module.exports = function(config) {
-    if (!config || !config.projectId) {
-        throw new Error('projectId is required.');
+  if (!config || !config.projectId) {
+    throw new Error('projectId is required.');
+  } else if (!config.instance) {
+    throw new Error('instance is required.');
+  } else if (!config.column_family) {
+    throw new Error('column family is required.');
+  } else if (!config.table) {
+    throw new Error('table is required.');
+  }
+
+  bigtable = require('@google-cloud/bigtable')({
+    projectId: config.projectID,
+  });
+
+  var instance = bigtable.instance(config.instance);
+  var table = instance.table(config.table);
+  var cf = config.column_family;
+
+  console.log('--------------------');
+  console.log(config);
+  console.log('--------------------');
+
+  return {
+    teams: {
+      get: get(table, cf, 'team'),
+      save: save(table, cf, 'team'),
+      all: all(table, cf, 'team'),
+      remove: remove(table, cf, 'team')
+    },
+    channels: {
+      get: get(table, cf, 'chan'),
+      save: save(table, cf, 'chan'),
+      all: all(table, cf, 'chan')
+    },
+    users: {
+      get: get(table, cf, 'user'),
+      save: save(table, cf, 'user'),
+      all: all(table, cf, 'user')
     }
-
-    var datastore, table, cf;
-
-    // var datastore = gcloud.datastore(config),
-    //     namespace = config.namespace,
-    //     teamKind = config.teamKind || 'BotkitTeam',
-    //     channelKind = config.channelKind || 'BotkitChannel',
-    //     userKind = config.userKind || 'BotkitUser';
-
-    return {
-        teams: {
-            get: get(datastore, table, cf),
-            save: save(datastore, table, cf),
-            all: all(datastore, table, cf)
-        },
-        channels: {
-            get: get(datastore, table, cf),
-            save: save(datastore, table, cf),
-            all: all(datastore, table, cf)
-        },
-        users: {
-            get: get(datastore, table, cf),
-            save: save(datastore, table, cf),
-            all: all(datastore, table, cf)
-        }
-    };
+  };
 };
 
 /**
@@ -51,30 +64,33 @@ module.exports = function(config) {
  * @param {String} kind The entity kind
  * @returns {Function} The get function
  */
-function get(datastore, kind, namespace) {
-    return function(id, cb) {
-      var team = {}
-      team.id = 123
-      team = JSON.stringify(team);
+function get(table, family, column) {
 
-      return cb(null, team);
-        // var keyParam = [kind, id];
-        // if (namespace) {
-        //     keyParam = {
-        //         namespace: namespace,
-        //         path: keyParam
-        //     };
-        // }
-        // var key = datastore.key(keyParam);
+  var getRow = function(table, id) {
+    var deferred = Q.defer();
 
-        // datastore.get(key, function(err, entity) {
-        //     if (err)
-        //         return cb(err);
+    var row = table.row(id);
+    row.get(id, function(err, data) {
+      if (err) {
+        deferred.reject();
+      } else {
+        deferred.resolve(JSON.parse(data.data[family][column][0].value));
+      }
+    });
 
-        //     return cb(null, entity ? entity : null);
-        // });
-    };
-};
+    return deferred.promise;
+  };
+
+  var get = function(id, cb) {
+    getRow(table, id).then(function(data) {
+      cb(data, null);
+    }, function() {
+      cb(null, null);
+    });
+  };
+
+  return get;
+}
 
 /**
  * Given a datastore reference and a kind, will return a function that will save an entity.
@@ -84,89 +100,83 @@ function get(datastore, kind, namespace) {
  * @param {String} kind The entity kind
  * @returns {Function} The save function
  */
-function save(datastore, kind, namespace) {
+function save(table, family, column) {
     return function(data, cb) {
-        var keyParam = [kind, data.id];
-        if (namespace) {
-            keyParam = {
-                namespace: namespace,
-                path: keyParam
-            };
+
+      var r = {};
+      r.key = data.id;
+      r.data = {};
+      r.data[family] = {};
+      r.data[family][column] = JSON.stringify(data);
+
+      // Needs error handling !!!!! //
+      table.insert([r], function(err) {
+        if (err) {
+          console.log(err);
         }
-        var key = datastore.key(keyParam);
-        datastore.save({
-            key: key,
-            // @TODO: convert object to array so that we can exclude properties from indexes
-            // data: [
-            //   {
-            //     name: 'rating',
-            //     value: 10,
-            //     excludeFromIndexes: true
-            //   }
-            // ]
-            data: data
-        }, cb);
+      });
     };
-};
-
-var getRows = function() {
-
 }
 
 /**
- * Given a datastore reference and a kind, will return a function that will return all stored entities.
+ * Given an id, table and kind, will return a function that will delete the row.
+ * The object must have an id property
  *
  * @param {Object} datastore A reference to the datastore Object
  * @param {String} kind The entity kind
- * @returns {Function} The all function
+ * @returns {Function} The save function
  */
-function all(datastore, kind, namespace) {
+function remove(table, family, column) {
+    return function(id, cb) {
+      var row = table.row(id);
+      row.delete(function(err, apiResponse) {});
+    };
+}
 
-  var instance = bigtable.instance('instance-1');
-  var table = instance.table('PCKLISS');
+/**
+ * Given a bigtable table and cf, this will return a function that will return all stored teams.
+ */
+function all(table, cf, column) {
 
-  var egg = function() {
+  var getRows = function(table, family, column) {
     var deferred = Q.defer();
-    table.getRows(function(err, rows) {
-      console.log('===========================')
-      console.log(rows)
-      deferred.resolve();
+
+    var filter = [
+      {
+        column: {
+          family: column,
+          cellLimit: 1
+        }
+      },
+      {
+        family: family
+      }
+    ];
+
+    var opts = {
+      filter: filter
+    };
+
+    var result = [];
+    table.getRows(opts).then(function(data) {
+      for (var i = 0, len = data[0].length; i < len; i++) {
+        result.push(JSON.parse(data[0][i].data[family][column][0].value));
+      }
+
+      if (result.length === data[0].length) {
+        deferred.resolve(result);
+      }
+    });
+    return deferred.promise;
+  };
+
+  var all = function(cb) {
+    getRows(table, cf, column).then(function(data) {
+      cb(null, data);
+    }, function() {
+      cb(null, null);
     });
 
-    // table.getRows().then(function(data) {
-    //   rows = data[0];
-    //   console.log(rows)
-    // }, function() {
-    //   deferred.resolve();
-    // });
-
-    return deferred.promise;
-  }
-
-    var test = function(cb) {
-      var teams = [];
-
-
-      var team = {}
-      team.id = 123
-      teams.push(team);
-
-      var rows;
-
-      egg().then(function() {
-        cb(null, teams);
-      }, function() {
-        cb(null, teams);
-      })
-
-
-      // table.getRows(function(err, rows) {
-      //   // if (!err) {
-
-      //     // `rows` is an array of Row objects.
-      //   // }
-      // });
-
-    };
-    return test;
-};
+  };
+  return all;
+}
